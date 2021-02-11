afa5bc61b13bc6a1fa2262a80823f64238fc4bcd
tomvdmade <tomvdmade@gmail.com>
2021-02-11 14:28:25 +0100

    lpthw

---
+++ ex3.py
@@ -0,0 +1,33 @@
+# outputs text string
+print("I will now count my chickens.")
+
+# outputs text followed by calculation 30/5 = 5, 25 + 5 = 30
+print("Hens", 25 + 30 / 6)
+# outputs text followed by calculation first 25 * 3 = 75, then 75 mod(ulo) 4 = 75 / 4 = 18.75, 18 * 4 = 72, 75 - 72 = 3. So 100 -3 = 97.
+print("Roosters", 100 - 25 * 3 % 4)
+
+
+# outputs text string
+print("Now I will count the eggs:")
+
+# first 4 mod 2 = 0 (4/2=2,2*2=4, 4-4=0) and  1/4=0.25 , first half is 6 - 5 = 1 - 0.25 = 0.75 + 6 = 6.75
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# outputs text string
+print("Is it true that 3 + 2 < 5 -7?")
+
+# calculates 5 is less then -2 which is false
+print(3 + 2 < 5 - 7)
+
+# print("What is 3 +2?", 3 + 2)
+# print("What is 5 - 7?", 5 - 7)
+
+# print("Oh, that's why its's False.")
+
+# print("How about some more.")
+
+# print("Is it greater?", 5 > -2)
+# print("Is it greater or equal?", 5>= -2)
+# print("Is it less or equal?", 5 <= -2)
+
+
